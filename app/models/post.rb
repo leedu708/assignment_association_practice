fc632f5391a1ed405efdad61948148c230401039
leedu708 <djl265@cornell.edu>
2015-10-07 15:32:04 -0400

    edited destroy methods

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,24 +1,10 @@
 class Post < ActiveRecord::Base
 
   belongs_to :category
-  has_many :comments
-  has_many :post_authorings, :class_name => 'UserPosting'
+  has_many :comments, dependent: :destroy
+  has_many :post_authorings, :class_name => 'UserPosting', dependent: :destroy
   has_many :authors, :through => :post_authorings, :class_name => 'User', :source => :user
-  has_many :post_taggings
+  has_many :post_taggings, dependent: :destroy
   has_many :tags, :through => :post_taggings
 
-  before_destroy :destroy_comments, :destroy_post_authorings, :destroy_post_taggings
-
-  def destroy_comments
-    comments.destroy_all
-  end
-
-  def destroy_post_authorings
-    post_authorings.destroy_all
-  end
-
-  def destroy_post_taggings
-    post_taggings.destroy_all
-  end
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,10 @@
 class User < ActiveRecord::Base
 
-  has_many :post_authorings, :class_name => 'UserPosting'
+  has_many :post_authorings, :class_name => 'UserPosting', dependent: :destroy
   has_many :authored_posts, :through => :post_authorings, :class_name => 'Post', :source => :post
-  has_many :authored_comments, :class_name => 'Comment'
+  has_many :authored_comments, :class_name => 'Comment', dependent: :destroy
 
   has_many :post_taggings, :through => :authored_posts
   has_many :tags_on_authored_posts, :through => :post_taggings, :class_name => 'Tag', :source => :tag
-
-  before_destroy :destroy_comments, :destroy_post_authorings
-
-  def destroy_comments
-    authored_comments.destroy_all
-  end
-
-  def destroy_post_authorings
-    post_authorings.destroy_all
-  end
   
 end
